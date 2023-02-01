7e16918614bebb0263059bc1d6c72cb91235bd5c
Ashpreet Bedi <ashpreet@phidata.com>
2023-02-01 14:44:33 -0500

    update

--- workspace/settings.py
+++ workspace/settings.py
@@ -14,14 +14,14 @@ ws_settings = WorkspaceSettings(
     dev_env="dev",
     # -*- Dev Apps
     dev_api_server_enabled=True,
-    dev_postgres_enabled=True,
-    dev_redis_enabled=True,
+    # dev_postgres_enabled=True,
+    # dev_redis_enabled=True,
     # -*- Production settings
     prd_env="prd",
     # -*- Production Apps
     prd_api_server_enabled=True,
-    prd_postgres_enabled=True,
-    prd_redis_enabled=True,
+    # prd_postgres_enabled=True,
+    # prd_redis_enabled=True,
     # -*- AWS settings
     # Region for AWS resources
     aws_region="us-east-1",
